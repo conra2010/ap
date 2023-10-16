e7ea0f076d2c22fba9c8191f9fa4ab43157ac39c
Conrado Andreu Capdevila <conrado.andreu@gmail.com>
2023-10-16 14:31:10 +0200

    step ca docs

--- api/src/Story/DefaultPostsStory.php
+++ api/src/Story/DefaultPostsStory.php
@@ -10,6 +10,6 @@ final class DefaultPostsStory extends Story
     public function build(): void
     {
         // TODO build your story here (https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#stories)
-        PostFactory::createMany(10);
+        PostFactory::createMany(256);
     }
 }
